0d0cd2643147fe8efb4bccb9cc1d532527ee8c29
Renan Pereira <renanmol87@gmail.com>
2021-12-27 20:10:26 -0300

    feat(type): create infer query type

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,13 @@
 import * as trpcNext from '@trpc/server/adapters/next'
-import { appRouter } from 'backend/router'
+import { inferProcedureOutput } from '@trpc/server'
+import { appRouter, AppRouter } from 'backend/router'
 
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => null,
 })
+
+export type inferQueryResponse<
+  TRouteKey extends keyof AppRouter['_def']['queries']
+> = inferProcedureOutput<AppRouter['_def']['queries'][TRouteKey]>
